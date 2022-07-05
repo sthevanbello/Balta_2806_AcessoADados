2130b6b177d67092d449ce27542f3797de0f7b37
sthevanbello <sthevan.alves@gmail.com>
2022-07-05 19:01:53 -0300

    Instalação do Dapper 2.0.90

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace DataAccess
             //const string connectionString = "Server=localhost,1433;Database=DB_Balta;Encrypt=False;Trusted_Connection=False;User ID=sa;Password=1q2w3e4r@#$";
             // 1q2w3e4r@#$
             // Encrypt=False - Falta isso para funcionar depois Microsoft.Data.SqlClient versão 4.0.0;
+            // Trusted_Connection=False; TrustServerCertificate=True
             // Microsoft.Data.SqlClient (NUGET)
 
             //var connection = new SqlConnection(connectionString);
@@ -19,21 +20,8 @@ namespace DataAccess
             //connection.Close();
             using (var connection = new SqlConnection(connectionString))
             {
-                connection.Open();
-                using (var command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                    }
-                }
+                
             }
-
-
         }
     }
 }
