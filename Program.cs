1fb84bd34f3f34e92fcbf4a315e955aec0a6d9f4
sthevanbello <sthevan.alves@gmail.com>
2022-07-05 19:01:53 -0300

    INstalação do Dapper 2.0.90

--- Program.cs
+++ Program.cs
@@ -19,21 +19,8 @@ namespace DataAccess
             //connection.Close();
             using (var connection = new SqlConnection(connectionString))
             {
-                connection.Open();
-                using (var command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                    }
-                }
+                
             }
-
-
         }
     }
 }
