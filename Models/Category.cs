b234a7c352a91649b9675169b40eef815c574a53
sthevanbello <sthevan.alves@gmail.com>
2022-07-07 20:44:21 -0300

    Criada uma categoria e inserida no banco utilizando o Dapper

--- Models/Category.cs
+++ Models/Category.cs
@@ -10,6 +10,10 @@ namespace DataAccess.Models
     {
         public Guid Id{ get; set; }
         public string Title { get; set; }
-
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public int Order { get; set; }
+        public string Description { get; set; }
+        public bool Featured { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,26 +10,51 @@ namespace DataAccess
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=DB_Balta;Trusted_Connection=False; TrustServerCertificate=True;User ID=sa;Password=1q2w3e4r@#$";
+            
             //const string connectionString = "Server=localhost,1433;Database=DB_Balta;Encrypt=False;Trusted_Connection=False;User ID=sa;Password=1q2w3e4r@#$";
             // 1q2w3e4r@#$
             // Encrypt=False - Falta isso para funcionar depois Microsoft.Data.SqlClient versão 4.0.0;
             // Trusted_Connection=False; TrustServerCertificate=True
             // Microsoft.Data.SqlClient (NUGET)
+            const string sql = "SELECT [Id], [Title] FROM [Category]";
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+            var insertSql = @"INSERT INTO [Category] VALUES (
+                                @Id, 
+                                @Title, 
+                                @Url, 
+                                @Summary, 
+                                @Order, 
+                                @Description, 
+                                @Featured)";
 
-            //var connection = new SqlConnection(connectionString);
-            //connection.Open();
-
-            //connection.Close();
             using (var connection = new SqlConnection(connectionString))
             {
-                var sql = "SELECT [Id], [Title] FROM [Category]";
-
-                var categories = connection.Query<Category>(sql);
-                foreach (var category in categories)
+                var rows = connection.Execute(insertSql, new
                 {
-                    Console.WriteLine($"Id: {category.Id} - Title: {category.Title}");
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"Rows: {rows}");
+                var categories = connection.Query<Category>(sql);
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"Id: {item.Id} - Title: {item.Title}");
                 }
             }
         }
+
+        
     }
 }
